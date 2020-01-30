ca24e07316c8b0ca76bdef7157c3a11c76d6bf23
VSolntseva <veron_SVA@outlook.com>
2020-01-30 18:26:01 +0300

    Fields in DbObjects

--- DAL/DbObjects/Group.cs
+++ DAL/DbObjects/Group.cs
@@ -4,7 +4,13 @@ using System.Text;
 
 namespace DAL.DbObjects
 {
-    class Group
+    public class Group
     {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+
+        public ICollection<Student> Students { get; set; }
     }
 }
--- DAL/DbObjects/Student.cs
+++ DAL/DbObjects/Student.cs
@@ -4,7 +4,21 @@ using System.Text;
 
 namespace DAL.DbObjects
 {
-    class Student
+    public class Student
     {
+        public int Id { get; set; }
+
+        public int Gender { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Name { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string UniqueName { get; set; }
+
+
+        public ICollection<Group> Groups { get; set; }
     }
 }
